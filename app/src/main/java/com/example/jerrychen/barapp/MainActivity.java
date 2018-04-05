276b6a6cca21a7c471c4505585b2b9546ac0d29d
DJ <DJ>
2018-04-05 09:12:21 +0200

    sjxsjxcijxc

--- app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
+++ app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
@@ -1,13 +1,34 @@
 package com.example.jerrychen.barapp;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawLay;
+    private ActionBarDrawerToggle mToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawLay = (DrawerLayout) findViewById(R.id.drawer);
+        mToggle = new ActionBarDrawerToggle(this, mDrawLay, R.string.Open, R.string.Close);
+        mDrawLay.addDrawerListener(mToggle);
+        mToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
+
