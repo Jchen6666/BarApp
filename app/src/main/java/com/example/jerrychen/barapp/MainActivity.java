e973d5201f96ccbbfbf73f116e6ec348f8d19ba0
cj154014737 <cj154014737@hotmail.com>
2018-04-08 16:59:58 +0200

    firebase authentication setup

--- app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
+++ app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.jerrychen.barapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+   private EditText editTextEmail,editTextPassword;
+    private FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
+        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        firebaseAuth=FirebaseAuth.getInstance();
     }
+
+    public void buttonClickRegister(View view) {
+        String email=editTextEmail.getText().toString().trim();
+        String password=editTextPassword.getText().toString().trim();
+        firebaseAuth.createUserWithEmailAndPassword("Jerry@gmail.com","12345");
+
+    }
+
+
 }
