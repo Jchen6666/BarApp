4e295c5f884e6e8c22a321b641afe8cc9d8ea202
Mads <wowgamecardadmin@gmail.com>
2018-04-09 12:59:37 +0200

    login test

---
+++ app/src/main/java/MadsTestLogin.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Mads on 26-03-2018.
+ */
+import java.util.*;
+import android.*;
+
+public class MadsTestLogin {
+
+        private static final String TAG = "LoginActivity";
+        private static final int REQUEST_SIGNUP = 0;
+
+
+    public static void main(String[]argv){
+    }
+}
---
+++ app/src/main/java/Username.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Mads on 26-03-2018.
+ */
+
+
+public class Username {
+
+    private
+}
