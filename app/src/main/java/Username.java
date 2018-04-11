c03c46b38729f325785ca52e3a0d71e33a8b9d95
Mads <wowgamecardadmin@gmail.com>
2018-04-11 16:10:52 +0200

    login test

--- app/src/main/java/Username.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Mads on 26-03-2018.
- */
-
-
-public class Username {
-
-    private
-}
--- app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
+++ app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
@@ -7,38 +7,50 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-   private EditText editTextEmail,editTextPassword;
+    private EditText editTextEmail, editTextPassword, editTextAge;
     private FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
-        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
-        firebaseAuth=FirebaseAuth.getInstance();
+        editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        editTextAge = (EditText) findViewById(R.id.editTextAge);
+        firebaseAuth = FirebaseAuth.getInstance();
     }
+
 
     public void buttonClickRegister(View view) {
-        String email=editTextEmail.getText().toString().trim();
-        String password=editTextPassword.getText().toString().trim();
-        firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()){
-                    Toast.makeText(MainActivity.this,"Registered Successfully",Toast.LENGTH_LONG).show();
-                }else {
-                    Toast.makeText(MainActivity.this,"Registered failed",Toast.LENGTH_LONG).show();
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+        int age = Integer.parseInt(editTextAge.getText().toString().trim());
+        if (age >= 18) {
+            firebaseAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(MainActivity.this, "Registered Successfully", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Registered failed", Toast.LENGTH_LONG).show();
+                    }
                 }
-            }
-        });
+            });
+        } else
+            Toast.makeText(MainActivity.this,"Under 18 not allowed", Toast.LENGTH_LONG).show();
+
 
     }
-
-
 }
+
+
+
+
+
