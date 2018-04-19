bad3363a05acd852669ffafc8bf2e4aeb3ba8064
jerrychen <jerry>
2018-04-19 16:35:25 +0200

    read data from firebase

--- app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
+++ app/src/main/java/com/example/jerrychen/barapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.example.jerrychen.barapp;
 
 import android.content.Intent;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -14,8 +16,14 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
    private EditText editTextEmail,editTextPassword;
@@ -23,13 +31,13 @@ public class MainActivity extends AppCompatActivity {
     private String email,password;
     private FirebaseDatabase database;
     private DatabaseReference dbRef;
-
+    private String uid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editTextEmail=(EditText)findViewById(R.id.editTextEmail);
-        editTextPassword=(EditText)findViewById(R.id.editTextPassword);
+        editTextEmail=findViewById(R.id.editTextEmail);
+        editTextPassword=findViewById(R.id.editTextPassword);
         firebaseAuth=FirebaseAuth.getInstance();
         database=FirebaseDatabase.getInstance();
         dbRef=database.getReferenceFromUrl("https://barapp-de43a.firebaseio.com/User");
@@ -45,8 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 if (task.isSuccessful()){
                     Toast.makeText(MainActivity.this,"Registered Successfully",Toast.LENGTH_LONG).show();
                     FirebaseUser user=firebaseAuth .getCurrentUser();
-                   // Toast.makeText(MainActivity.this,user.getUid(),Toast.LENGTH_LONG).show();
-                    registerInDb(user.getUid());
+                  //  Toast.makeText(MainActivity.this,user.getUid(),Toast.LENGTH_LONG).show();
+                    if (user.getUid().isEmpty()==false) {
+                        registerInDb(user.getUid());
+                    }else {
+                        Toast.makeText(MainActivity.this,"cannot get data",Toast.LENGTH_LONG).show();
+                    }
 
                 }else {
                     Toast.makeText(MainActivity.this,"Registered failed",Toast.LENGTH_LONG).show();
@@ -69,6 +81,9 @@ public class MainActivity extends AppCompatActivity {
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
                                 Toast.makeText(MainActivity.this, "Login Successfully", Toast.LENGTH_LONG).show();
+                                FirebaseUser user=firebaseAuth .getCurrentUser();
+                                uid=user.getUid();
+                                readDb(uid);
                             } else {
                                 Toast.makeText(MainActivity.this, "Wrong email or password", Toast.LENGTH_LONG).show();
                             }
@@ -76,6 +91,29 @@ public class MainActivity extends AppCompatActivity {
                     });
 
         }
+    }
+    public void readDb(String uid){
+        DatabaseReference userDb=database.getReferenceFromUrl("https://barapp-de43a.firebaseio.com/User/"+ uid);
+        Log.d("D_value", "URL "+"https://barapp-de43a.firebaseio.com/User/"+ uid);
+       userDb.addValueEventListener(new ValueEventListener() {
+           @Override
+           public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String,String> map=(Map<String, String>) dataSnapshot.getValue();
+                String name=map.get("name");
+                String age=map.get("age");
+                String email=map.get("email");
+
+                Log.d("D_value", "name "+name);
+                Log.d("D_value", "age "+age);
+                Log.d("D_value", "email "+email);
+
+           }
+
+           @Override
+           public void onCancelled(DatabaseError databaseError) {
+
+           }
+       });
     }
     public void registerInDb(String uid){
         DatabaseReference childRef=dbRef.child(uid);
@@ -85,5 +123,10 @@ public class MainActivity extends AppCompatActivity {
 
     public void buttonClickDbTest(View view) {
         registerInDb("qwjdoiqwjd");
+    }
+
+    public void buttonClickNavigation(View view) {
+        Intent intent=new Intent(this,LoginActivity.class);
+        startActivity(intent);
     }
 }
