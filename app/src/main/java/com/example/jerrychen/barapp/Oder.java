d483bc00ed84885b1800a7c9b94fccd75cc9cca3
jerrychen <jerry>
2018-05-03 13:39:36 +0200

    Registration and login page

---
+++ app/src/main/java/com/example/jerrychen/barapp/Oder.java
@@ -0,0 +1,12 @@
+package com.example.jerrychen.barapp;
+
+import java.sql.Time;
+import java.util.Date;
+
+/**
+ * Created by jerrychen on 5/2/18.
+ */
+
+public class Oder {
+    private Date date;
+}
