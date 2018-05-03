f14e0b8a4ad9884520b32663fd356935c27a2540
jerrychen <jerry>
2018-05-03 14:03:21 +0200

    try to checkout

--- app/src/main/java/com/example/jerrychen/barapp/Oder.java
+++ app/src/main/java/com/example/jerrychen/barapp/Oder.java
@@ -2,6 +2,7 @@ package com.example.jerrychen.barapp;
 
 import java.sql.Time;
 import java.util.Date;
+import java.util.Map;
 
 /**
  * Created by jerrychen on 5/2/18.
@@ -9,4 +10,6 @@ import java.util.Date;
 
 public class Oder {
     private Date date;
+    private Map<String,Integer> odermap;
+
 }
